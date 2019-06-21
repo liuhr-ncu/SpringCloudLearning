d1e4949241cdad552316b3830430b7483ac6945e
liuhr <2648410107@qq.com>
2019-06-21 17:24:43 +0800

    修复一些缺少maven依赖的问题

--- chapter4/service-feign/src/main/java/com/forezp/service/SchedualServiceHi.java
+++ chapter4/service-feign/src/main/java/com/forezp/service/SchedualServiceHi.java
@@ -1,6 +1,7 @@
 package com.forezp.service;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,8 +9,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 /**
  * Created by fangzhipeng on 2017/4/6.
  */
-@FeignClient(value = "service-hi",fallback = SchedualServiceHiHystric.class)
+@FeignClient(value = "service-hi",fallback = SchedualServiceHi.SchedualServiceHiHystric.class)
 public interface SchedualServiceHi {
     @RequestMapping(value = "/hi",method = RequestMethod.GET)
     String sayHiFromClientOne(@RequestParam(value = "name") String name);
+
+    @Component
+    class SchedualServiceHiHystric implements SchedualServiceHi {
+        @Override
+        public String sayHiFromClientOne(String name) {
+            return "sorry "+name;
+        }
+    }
+
 }
--- chapter4/service-feign/src/main/java/com/forezp/web/HiController.java
+++ chapter4/service-feign/src/main/java/com/forezp/web/HiController.java
@@ -2,10 +2,13 @@ package com.forezp.web;
 
 import com.forezp.service.SchedualServiceHi;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * Created by fangzhipeng on 2017/4/6.
@@ -15,6 +18,7 @@ public class HiController {
 
     @Autowired
     SchedualServiceHi schedualServiceHi;
+
     @RequestMapping(value = "/hi",method = RequestMethod.GET)
     public String sayHi(@RequestParam String name){
         return schedualServiceHi.sayHiFromClientOne(name);
